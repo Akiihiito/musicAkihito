852a6034ffd5fc61a991c5046b4a9b6f779c5413
Akiihiito <43644199+Akiihiito@users.noreply.github.com>
2018-09-27 16:26:19 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -19,4 +19,15 @@ bot.on('message', message=> {
         message.reply("_Ce bot a été crée par Akihito_");
         console.log("Commande fabriquant éfféctué");
     }
+    
+    if (message.content === prefix + "invite"){
+        var embed = newDiscord.RichEmbed()
+        .setTitle("Discord Akihito")
+        .setDescription("Voici le lien du discord")
+        .addFiel(".help","Page d'aide", true)
+        .addFiel("Embed01","Embed 01 ! Pour avoir des informations ou une aide [Discord Akihito](https://discord.gg/VxCb2pm)", true)
+        .setColor("0xFF0000")
+        .setFooter("Bievenue à vous ! ")
+        message.channel.sendEmbed(embed);
+    }
 });
