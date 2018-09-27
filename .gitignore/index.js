c5919c44138e8829eb75efdfba9d77bd271370c6
Akiihiito <43644199+Akiihiito@users.noreply.github.com>
2018-09-27 16:01:58 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -12,8 +12,8 @@ bot.login("NDk0ODMzNzU2MzMxNTczMjQ4.Do5mlg.GPJBcRzqkUc7SvMXQl857B_Gv_w");
 
 bot.on('message', message=> {
     if (message.content === prefix + "help"){
-        message.channel.sendMessage("Liste des commandes: \n -*help");
-    }
+        message.channel.sendMessage("Liste des commandes: \n *help \n *fabriquant");
+    
 
     if (message.content === prefix + "fabriquant"){
         message.reply("_Ce bot a été crée par Akihito_");
